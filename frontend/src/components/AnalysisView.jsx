51f35fd76ab3d33160205294fab1b74b5723a0a6
akirademoss <akirademoss@gmail.com>
2024-06-03 20:19:29 -0700

    starting file upload part of app

--- frontend/src/components/AnalysisView.jsx
+++ frontend/src/components/AnalysisView.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from 'react'
 import './AnalysisView.css'
+import { useForm } from "react-hook-form";
 
 function AnalysisView() {
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <>
 
-            <view className='analysis-view'>
-                <ul className='grid-container'>
-                    <div className='file-upload'>
-                        <h3>Upload a file and our analytics software will analyze the player's performance.</h3>
+            <div className='analysis-view'>
+                <div className='grid-container'>
+                    
+                    <div className='upload-container'>
+                        <div className='button-container'>
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <input {...register("picture", { required: true })} type="file" />
+                                <button>Submit</button>
+                            </form>
+                  
+                        </div>
+                        <div className='file-upload'>
+                            <h3>Upload a file and our analytics software will analyze the player's performance.</h3>
+                        </div>
                     </div>
+                    
                     <div className='analytics-container'>
                         <div className='analytics-box'>
                             <div className='performance-box'>
@@ -18,8 +36,8 @@ function AnalysisView() {
 
                         </div>
                     </div>
-                </ul>
-            </view>
+                </div>
+            </div>
 
         </>
     )
