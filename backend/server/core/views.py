bf4f1439276410edb727b0501c071e71b86f57ec
Akira DeMoss <akirademoss@gmail.com>
2024-06-09 14:49:47 -0700

    removed unneeded imports

--- backend/server/core/views.py
+++ backend/server/core/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView, ListView, CreateView
+from django.shortcuts import render
+from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
-from django.urls import reverse_lazy
+
 
 
 class Home(TemplateView):
