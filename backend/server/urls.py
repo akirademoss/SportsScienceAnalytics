14047b053de7ddd5dfc83cbe92fc3fb15fd6d9e6
Akira DeMoss <akirademoss@gmail.com>
2024-06-09 12:26:25 -0700

    updated project to include a simple file upload on backend with html manual test

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
+from server.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.Home.as_view(), name='home'),
+    path('upload/', views.upload, name='upload')
 ]
