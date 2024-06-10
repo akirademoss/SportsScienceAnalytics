360050686ef8e7001c110925cffc6f5947fd62ca
Akira DeMoss <akirademoss@gmail.com>
2024-06-09 19:20:58 -0700

    tested django rest framework

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -39,8 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'server.core',
+    'server',
+
+    'rest_framework',
+    'corsheaders',
+    'posts'
+
 ]
 
+CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
