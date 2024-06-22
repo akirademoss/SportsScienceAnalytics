e692b3a41dbd244237d204e74d7aacfa5a778630
akirademoss <akirademoss@gmail.com>
2024-06-22 14:54:28 -0700

    started adding logic for python file processing

--- backend/posts/api/views.py
+++ backend/posts/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
+import os
 from ..models import Score
 from .serializers import ScoreSerializer
 from django.core.files.storage import FileSystemStorage
@@ -17,4 +18,6 @@ class UploadView(APIView):#request
         fs = FileSystemStorage()
         name = fs.save(file_obj.name, file_obj)
         context['url'] = fs.url(name)
+        fpath = os.getcwd() + context['url']
+        print(fpath)
         return Response(status=200)
