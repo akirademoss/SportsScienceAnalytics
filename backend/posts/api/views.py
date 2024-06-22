4d97955ade3e9559dee3356fbae388b0ad5289cb
akirademoss <akirademoss@gmail.com>
2024-06-22 15:10:50 -0700

    added pandas packages to requirements.txt functionality for calculating mean heartrate and removing file after calculation added

--- backend/posts/api/views.py
+++ backend/posts/api/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 import os
+import pandas as pd
+import numpy as np
 from ..models import Score
 from .serializers import ScoreSerializer
 from django.core.files.storage import FileSystemStorage
@@ -20,4 +22,21 @@ class UploadView(APIView):#request
         context['url'] = fs.url(name)
         fpath = os.getcwd() + context['url']
         print(fpath)
+        score = extract_score(fpath)
+        print(score)
+        os.remove(fpath)
         return Response(status=200)
+
+#Converts a file to csv format
+def extract_score(fpath): 
+    df = pd.read_csv(fpath)
+    print(df.head(3))
+    arr = df["Value"].to_numpy()
+    avg_heart_rate = np.mean(arr)
+    print(avg_heart_rate)
+    if avg_heart_rate > 78:
+        score = "Excellent"
+    else:
+        score = "Good"
+    return score
+    
